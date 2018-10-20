8d63c3715236e0d1fc5f3b011e711707472c8941
Anwesh43 <anweshthecool0@gmail.com>
2018-10-20 12:06:38 +0530

    adding DStepView in Activity

--- app/src/main/java/ui/anwesome/com/linkeddstepview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/linkeddstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.linkeddstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.dstepview.DStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DStepView.create(this)
     }
 }
