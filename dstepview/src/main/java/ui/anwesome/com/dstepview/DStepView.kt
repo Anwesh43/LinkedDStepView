c7aa73259f77ce6f59de89e67c9c4b7ecfbc0416
Anwesh43 <anweshthecool0@gmail.com>
2018-10-20 11:58:58 +0530

    adding DStep class

--- dstepview/src/main/java/ui/anwesome/com/dstepview/DStepView.kt
+++ dstepview/src/main/java/ui/anwesome/com/dstepview/DStepView.kt
@@ -144,4 +144,26 @@ class DStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class DStep(var i : Int) {
+        private var curr : DStepNode = DStepNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
