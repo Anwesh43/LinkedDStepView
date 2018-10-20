4c36692385b50f7445707f1b2205de81b0e5cc53
Anwesh43 <anweshthecool0@gmail.com>
2018-10-20 11:55:28 +0530

    adding DStepNode class

--- dstepview/src/main/java/ui/anwesome/com/dstepview/DStepView.kt
+++ dstepview/src/main/java/ui/anwesome/com/dstepview/DStepView.kt
@@ -101,4 +101,47 @@ class DStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DStepNode(var i : Int, val state : State = State()) {
+        private var next : DStepNode? = null
+        private var prev : DStepNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = DStepNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDStep(i, state.scale, paint)
+            prev?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DStepNode {
+            var curr : DStepNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
