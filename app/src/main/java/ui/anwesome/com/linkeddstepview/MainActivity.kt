a48546bbb956b688312f60d350ff4b600e1f2944
Anwesh43 <anweshthecool0@gmail.com>
2018-10-20 12:15:25 +0530

    adding fullscreen support

--- app/src/main/java/ui/anwesome/com/linkeddstepview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/linkeddstepview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.linkeddstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.dstepview.DStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
